3e13ab34e28cd015c788a6af28b80d750b01af92
Slewog <31140215+Slewog@users.noreply.github.com>
2022-06-08 03:42:43 +0200

    remove dev command
    Delete the commands to show dialog during development

--- client.lua
+++ client.lua
@@ -39,19 +39,3 @@ local function CreateDialog(value)
 end
 
 exports("CreateDialog", CreateDialog)
-
-RegisterCommand('testDialog', function()
-    local options = {
-        icon = 'fas fa-car',
-        title = 'Premium Motorsport - Buy a car',
-        content = 'Do you want buy this Benefactor Schafter V12 for 25.0000$ ?'
-    }
-
-    local accepted = exports['esx_dialog']:CreateDialog(options)
-
-    if accepted then
-        exports['okokNotify']:Alert('Premium Motorsport : ', 'You are now the owner of this Benefactor Schafter V12 !', 2500, 'info')
-    else
-        exports['okokNotify']:Alert('Premium Motorsport : ', 'Too bad it will be for another time, have a nice day !', 2500, 'info')
-    end
-end)
