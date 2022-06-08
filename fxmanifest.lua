a65abf6b97f4ffe58c5c9101404649654e149fca
Slewog <31140215+Slewog@users.noreply.github.com>
2022-06-08 03:43:33 +0200

    add repo link

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,11 @@
 --[[ FX Information ]]--
 fx_version 'adamant'
--- use_fxv2_oal 'yes'
--- lua54 'yes'
 game 'gta5'
 
 name         'esx_dialog'
 author		 'Slewog'
 version      '1.0.0'
--- repository   ''
+repository   'https://github.com/Slewog/esx_dialog'
 description  'Display a dialog to the player to confirm or cancel an action'
 
 shared_script '@es_extended/imports.lua'
